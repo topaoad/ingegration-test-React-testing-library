e9dc0714ac5b458e3d841d84180e2d42751b902b
GomaGoma676 <pga.adam.scott@gmail.com>
2021-01-11 23:12:48 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+const Home: React.FC = () => {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Nextjs</p>
+    </Layout>
+  )
+}
+export default Home
